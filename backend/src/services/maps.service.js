dcecc15ba6e4ebea71504ba41c3cfb17d6135fec
tomasgallego <sabiojuancruzsecundario@gmail.com>
2026-05-18 23:59:46 -0300

    mismo commit de antes pero desde mi cuenta porque tenia la cuenta de galle de una vez que lo ayude en un tp

--- backend/src/services/maps.service.js
+++ backend/src/services/maps.service.js
@@ -2,6 +2,51 @@ const axios = require('axios')
 
 const getRoute = async (req, res) => {
   try {
+    //formato de req.body:
+    //{
+    //  "origin" (direccion origen):{
+    //    "location":{
+    //      "latLng" (coordenadas en latitud y longitud, se obtienen con getDirection):{
+    //        "latitude": 37.419734,
+    //        "longitude": -122.0827784
+    //      }
+    //    }
+    //  },
+    //  "destination" (direccion destino):{
+    //    "location":{
+    //      "latLng" (coordenadas en latitud y longitud, se obtienen con getDirection):{
+    //        "latitude": 37.417670,
+    //        "longitude": -122.079595
+    //      }
+    //    }
+    //  },
+    //    "intermediates" (destinos intermedios, es un array): [
+    //       {
+    //        "location":{
+    //          "latLng" (coordenadas en latitud y longitud, se obtienen con getDirection):{
+    //            "latitude": 37.417672,
+    //            "longitude": -122.079595
+    //          }
+    //        }
+    //       },
+    //       {
+    //        "location":{
+    //          "latLng" (coordenadas en latitud y longitud, se obtienen con getDirection):{
+    //            "latitude": 37.417671,
+    //            "longitude": -122.079595
+    //          }
+    //        }
+    //      }
+    //    ],
+    //  "travelMode" (modo de viaje, en caso de perrubi siempre sera WALK): "WALK",
+    //  "departureTime" (hora de salida, en formato timestamp): "2024-06-01T08:00:00Z",
+    //  "routeModifiers" (algunos modificadores de ruta): {
+    //    avoidIndoor (evita rutas por interiores): true,
+    //  },
+    // "optimizeWaypointOrder" (optimiza el orden de los waypoints (destinos intermedios), siempre en true): true,
+    //  "languageCode" (lenguaje): "en-US",
+    //  "units" (unidades, en este caso metros): "METRIC"
+    //}
 
     const response = await axios.post(
 
@@ -33,6 +78,11 @@ const getRoute = async (req, res) => {
 
 const getDirection = async (req, res) => {
   try {
+      //formato de req.body:
+      // {
+      //   "textQuery": Ubicacion a buscar, por ejemplo: "Galván 3124, CABA"
+      // }
+
     const place_info = await axios.post(
       'https://places.googleapis.com/v1/places:searchText',
       req.body,
@@ -57,11 +107,25 @@ const getDirection = async (req, res) => {
 
           'X-Goog-Api-Key': process.env.API_KEY_MAPS,
 
-          'X-Goog-FieldMask': 'name,formattedAddress,location'
+          'X-Goog-FieldMask': 'name,formattedAddress,location,displayName'
         }
       }
     )
     
+    //formato de respuesta:
+    //ejemplo galvan 3124, caba
+    // {
+    //   "name": "places/ChIJl4-lE_W2vJUR3RhS7NvnOVY",
+    //   "formattedAddress": "Galván 3124, C1431 Cdad. Autónoma de Buenos Aires, Argentina",
+    //   "location": {
+    //     "latitude": -34.5667227,
+    //     "longitude": -58.48777759999999
+    //   },
+    //   "displayName": {
+    //     "text": "Galván 3124"
+    //   }
+    // }
+
     res.json(response.data)
   } catch (error) {
     console.error(error.response?.data || error.message)
